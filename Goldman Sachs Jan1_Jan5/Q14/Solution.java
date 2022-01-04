89b42f1497b151c08b907c5e664f5fa4489472fe
Umang Agarwal <63835975+umang345@users.noreply.github.com>
2022-01-04 10:38:21 +0530

    Added Solution to Q14 for Goldman Sachs Jan1-Jan5

---
+++ Goldman Sachs Jan1_Jan5/Q14/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int minSubArrayLen(int target, int[] nums) 
+    {
+        int left = 0;
+        int sum = 0;
+        int result = Integer.MAX_VALUE;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+             sum+=nums[i];
+            
+            while(sum>=target)
+            {
+                 result = Math.min(result,i-left+1);
+                 sum-=nums[left];
+                left++;
+            }
+        }
+        return (result!=Integer.MAX_VALUE)? result : 0;
+        
+    }
+}
